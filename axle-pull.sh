3fdea643872a67b40ca099a87538925dea19d7a2
Adria Armejach <adria.armejach@bsc.es>
2014-03-10 17:15:31 +0100

    Add script to pull results from axle machine

---
+++ axle-pull.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BASEDIR=$(dirname "$0")
+BASEDIR=$(cd "$BASEDIR"; pwd)
+
+USER=adria
+SERVER=axle.bsc.es
+SERVER_HOME=/home/adria
+SERVER_DIR=$USER@$SERVER:$SERVER_HOME
+RESULTS_DIR=dbms-profiler/results
+
+rsync -aP $SERVER_DIR/$RESULTS_DIR .
+
+#ssh $SERVER $SERVER_HOME/script.sh
