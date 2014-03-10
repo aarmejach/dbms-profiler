82c5f56a9774b505c2b74540289bd0bbe24031ab
Adria Armejach <adria.armejach@bsc.es>
2014-03-10 14:34:04 +0100

    Restructuring the profiler infrastructure

---
+++ run_profiler.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+BASEDIR=$(dirname "$0")
+BASEDIR=$(cd "$BASEDIR"; pwd)
+
+source $BASEDIR/common/config
+
+show_help() {
+    echo "Execute benchmarks for certain databases."
+    echo "You can specify DBMS to be used with -db. Possible options are $DATABASES."
+}
+
+
+while test $# -gt 0
+do
+    case $1 in
+        -h|--help) # print help
+            show_help
+            exit 1
+            ;;
+        -db|--database) # DBMS to be used
+            DATABASES=$2
+            shift
+            ;;
+        -b|--benchmark) # bench to be used, if not specified all will be executed
+            BENCHMARKS=$2
+            shift
+            ;;
+        -p|--prepare) # just create the DB, don't run the benchmark
+            PREPARE=true
+            shift
+            ;;
+    esac
+    shift
+done
+
+for DATABASE in DATABASES; do
+
+    for bench in BENCHMARKS; do
+
+        # The DB folder must exist
+        if [ -d "$DATABASE" ]; then
+            echo "Load configuration for $DATABASE and $bench"
+            source "$BASEDIR/$DATABASE/$bench"
+        else
+            show_help
+            exit 0
+        fi
+
+        # Check if DB needs to be created and check if PREPARE flag is true
+        if [ -d "$DATADIR" ]; then
+            echo "DB folder exists, skip creation"
+        else
+            echo "DB does not exist, creating data dir in $DATADIR"
+            source "$BASEDIR/$DATABASE/create_database_$bench.sh"
+            echo "done creating data dir in $DATADIR"
+        fi
+        if [ "$PREPARE" = true ]; then
+            echo "Exiting, prepare flag was set to true"
+            exit 0
+        fi
+
+        # Will run the benchmark
+        echo "Get ready to run benchmarks: drop caches."
+        sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
+        sleep 5
+
+        echo "Running benchmark $bench for $DATABASE"
+        source "$BASEDIR/$DATABASE/run_$bench.sh"
+    done
+
+done
+
+
+echo "done."
+exit 1
