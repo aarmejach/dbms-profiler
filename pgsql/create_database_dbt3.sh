fc7e7a6336f268cdad2ba3adf37b928da835a25d
Adria Armejach <adria.armejach@bsc.es>
2014-04-09 11:36:19 +0200

    Add infrastructure to run dbt3 benchmark

---
+++ pgsql/create_database_dbt3.sh
@@ -0,0 +1,176 @@
+#!/bin/bash -e
+
+# Install teardown() function to kill any lingering jobs
+# Check if postgres is running
+source "$BASEDIR/$DATABASE/common.sh"
+
+# Current time
+t=$(timer)
+
+# Prepare benchmark
+echo "Prepare the benchmark: configure and make"
+cd "$BENCHDIR"
+sh autogen.sh
+./configure --with-postgresql=$PGPATH/local
+make
+
+# Initialize DATADIR
+mkdir -p "$DATADIR" || die "Failed to create directory $DATADIR"
+$PGBINDIR/initdb -D "$DATADIR" --encoding=UTF-8 --locale=C
+
+# Start a new instance of Postgres
+$PGBINDIR/postgres -D "$DATADIR" -p $PORT &
+PGPID=$!
+while ! $PGBINDIR/pg_ctl status -D $DATADIR | grep "server is running" -q; do
+  echo "Waiting for the Postgres server to start"
+  sleep 2
+done
+
+# Create database
+$PGBINDIR/createdb -h /tmp -p $PORT $DB_NAME --encoding=UTF-8 --locale=C
+
+# disable resolving of *.tbl to '*.tbl' in case there are no matching files
+shopt -s nullglob
+
+cd "$DBGENDIR"
+if ! [ -x dbgen ] || ! [ -x qgen ];
+then
+  make -j $CORES
+fi
+
+mkdir -p "$TPCHTMP" || die "Failed to create temporary directory: '$TPCHTMP'"
+cd "$TPCHTMP"
+cp "$DBGENDIR/dists.dss" .
+# Run dbgen with "force", to overwrite existing files
+# Create table files separately to have better IO throughput
+"$DBGENDIR/dbgen" -s $SCALE -f -v -T c &
+"$DBGENDIR/dbgen" -s $SCALE -f -v -T s &
+"$DBGENDIR/dbgen" -s $SCALE -f -v -T n &
+"$DBGENDIR/dbgen" -s $SCALE -f -v -T r &
+"$DBGENDIR/dbgen" -s $SCALE -f -v -T O &
+"$DBGENDIR/dbgen" -s $SCALE -f -v -T L &
+"$DBGENDIR/dbgen" -s $SCALE -f -v -T P &
+"$DBGENDIR/dbgen" -s $SCALE -f -v -T S &
+
+# Wait for all pending jobs to finish.
+for p in $(jobs -p);
+do
+  if [ $p != $PGPID ];
+  then
+      wait $p
+  fi
+done
+
+#echo "DROP DATABASE IF EXISTS $DB_NAME" | $PGBINDIR/psql -h /tmp -p $PORT
+# Make sure we're all on the same page wrt encoding, collations, etc.
+#$PGBINDIR/createdb -h /tmp -p $PORT $DB_NAME --encoding=UTF-8 --locale=C
+#if [ $? != 0 ]; then
+  # Did you forget to disconnect from the database before dropping?
+  #echo "Error: Can't proceed without database"
+  #exit -1
+#fi
+
+# Configuration for query execution
+query_configuration="
+checkpoint_segments = 300
+checkpoint_timeout = 3600s
+checkpoint_completion_target = 0.9
+checkpoint_timeout = $CHECKPOINT_TIMEOUT
+work_mem = $WORK_MEM                      #2 to 16GB
+effective_cache_size = $EFFECTIVE_CACHE        #3/4 of total RAM (192GB)
+default_statistics_target = $STATISTICS_TARGET    #requires analyze to take effect
+maintenance_work_mem = $MAINTAINANCE_MEM
+"
+echo "$query_configuration" | tee -a $DATADIR/postgresql.conf
+$PGBINDIR/pg_ctl reload -D $DATADIR
+
+
+TIME=`date`
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "comment on database $DB_NAME is 'DBT3 data, created at $TIME'"
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME < "$DBGENDIR/dss.ddl"
+
+cd "$TPCHTMP"
+for f in *.tbl; do
+  bf="$(basename $f .tbl)"
+  # We truncate the empty table in the sames transaction to enable Postgres to
+  # safely skip WAL-logging. See
+  # http://www.postgresql.org/docs/current/static/populate.html#POPULATE-PITR
+  echo "truncate $bf; COPY $bf FROM '$(pwd)/$f' WITH DELIMITER AS '|'" | $PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME &
+done
+
+# TODO: It would be nice if there was a way to limit the number of
+# concurrently executing jobs to $CORES. It is surprisingly easy to make COPY
+# CPU-bound.
+for p in $(jobs -p); do
+  if [ $p == $PGPID ]; then continue; fi
+  wait $p;
+done
+
+# Create primary and foreign keys
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME < "$DBGENDIR/dss.ri"
+
+# Remove tmp folder
+cd "$BASEDIR"
+rm -rf "$TPCHTMP"
+
+# Since wal_level is hopefully set to 'minimal', it ought to be possible to skip
+# WAL logging these create index operations, too.
+echo "Creating 'All' indexes..."
+# Primary key definitions already create indexes
+#$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_r_regionkey ON region (r_regionkey);" &
+#$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_n_nationkey ON nation (n_nationkey);" &
+#$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_p_partkey ON part (p_partkey);" &
+#$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_s_suppkey ON supplier (s_suppkey);" &
+#$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_c_custkey ON customer (c_custkey);" &
+#$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_o_orderkey ON orders (o_orderkey);" &
+
+# Pg does not create indexed on foreign keys, create them manually
+#$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_n_regionkey ON nation (n_regionkey);" & # not used
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_s_nationkey ON supplier (s_nationkey);" &
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_c_nationkey ON customer (c_nationkey);" &
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_ps_suppkey ON partsupp (ps_suppkey);" &
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_ps_partkey ON partsupp (ps_partkey);" &
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_o_custkey ON orders (o_custkey);" &
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_l_orderkey ON lineitem (l_orderkey);" &
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_l_suppkey_partkey ON lineitem (l_partkey, l_suppkey);" &
+
+# other indexes
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_l_shipdate ON lineitem (l_shipdate);" &
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_l_partkey ON lineitem (l_partkey);" &
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_l_suppkey ON lineitem (l_suppkey);" &
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_l_receiptdate ON lineitem (l_receiptdate);" &
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_l_orderkey_quantity ON lineitem (l_orderkey, l_quantity);" &
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_o_orderdate ON orders (o_orderdate);" &
+#$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "CREATE INDEX i_l_commitdate ON lineitem (l_commitdate);" & # not used
+
+for p in $(jobs -p); do
+    if [ $p == $PGPID ]; then continue; fi
+    wait $p;
+done
+
+# Disable transparent huge pages
+echo never | sudo tee /sys/kernel/mm/transparent_hugepage/defrag
+
+# Always analyze after bulk-loading; when hacking Postgres, typically Postgres
+# is run with autovacuum turned off.
+echo "Running vacuum freeze analyze..."
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "vacuum freeze"
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "analyze"
+# Checkpoint, so we have a "clean slate". Just in-case.
+echo "Checkpointing..."
+$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -c "checkpoint"
+
+echo "Stop the postgres server"
+$PGBINDIR/pg_ctl stop -D $DATADIR
+
+execution_configuration="
+track_activities = off
+track_counts = off
+track_io_timing = off
+update_process_title = off
+autovacuum = off
+"
+echo "$execution_configuration" | tee -a $DATADIR/postgresql.conf
+
+cd $BASEDIR
+printf 'Elapsed time: %s\n' $(timer $t)
---
+++ pgsql/run_dbt3.sh
@@ -0,0 +1,142 @@
+#!/bin/bash -e
+
+# Install teardown() function to kill any lingering jobs
+# Check if postgres is running
+source "$BASEDIR/$DATABASE/common.sh"
+
+mkdir -p $RESULTS
+cd $RESULTS
+
+# Start and Warmup
+echo "Start and Warmup: run all queries in succession"
+if [ "$SIMULATOR" = false ]; then
+    $PGBINDIR/postgres -D "$DATADIR" -p $PORT &
+    PGPID=$!
+    while ! $PGBINDIR/pg_ctl status -D $DATADIR | grep "server is running" -q; do
+        echo "Waiting for the Postgres server to start"
+        sleep 2
+    done
+
+    # Additional sleep time seems necessary
+    sleep 3
+
+    # Warmup: run all queries in succession
+    #echo "Warmup: run all queries in succession"
+    #/usr/bin/time -f '%e\n%Uuser %Ssystem %Eelapsed %PCPU (%Xtext+%Ddata %Mmax)k'\
+        #--output=warmup.exectime $PGBINDIR/psql -h /tmp\
+        #-p $PORT -d $DB_NAME < $QUERIESDIR/qall.sql  2> warmup.stderr > warmup.stdout
+fi
+
+
+RUNDIR=$RESULTS/run
+mkdir -p $RUNDIR
+
+# Create stream files and warmup
+i=1
+while [ $i -le $NUMSTREAMS ]
+do
+    query_file="$RUNDIR/throughput_query$i"
+    tmp_query_file="$RUNDIR/tmp_throughput_query$i.sql"
+    param_file="$RUNDIR/throughput_param$i"
+
+    # output PID to a tmp file
+    echo "$$" > $RUNDIR/PID$i
+
+    if [ ! -f $RUNDIR/$SEED_FILE ]; then
+        echo "creating seed file $SEED_FILE, you can change the seed by "
+        echo "modifying this file"
+        date +%-m%d%H%M%S > $RUNDIR/$SEED_FILE
+    fi
+    seed=`cat $RUNDIR/$SEED_FILE`
+    let "seed = $seed + $i"
+
+    # generate the queries for thoughput test
+    rm -f $query_file
+    rm -f $tmp_query_file
+    cd $DBGENDIR
+    DSS_QUERY=queries/$DATABASE ./qgen -c -r $seed -p $i -s $SCALE -l $param_file > $query_file
+    cd $RESULTS
+
+    # modify $query_file so that the commands are in one line
+    #${PARSE_QUERY} $query_file $tmp_query_file T $perf_run_num $stream_num
+
+    # get the execution plan for each query
+    #PLANDIR=$OUTPUT_DIR/db/plans
+    #mkdir -p $PLANDIR
+    #i=1
+    #while [ $i -le 22 ]
+    #do
+            #if [ $i -ne 15 ]; then
+                    #${DBSCRIPTDIR}/get_query_plan.sh ${scale_factor} ${i} \
+                                    #${PLANDIR}/throughput_stream${stream_num}_query${i}.txt \
+                                    #${RUNDIR} ${SEED_FILE} ${DBPORT}
+            #fi
+            #let "i=$i+1"
+    #done
+
+    # Warmup with first stream
+    echo "Warmup using stream $i"
+    /usr/bin/time -f '%e\n%Uuser %Ssystem %Eelapsed %PCPU (%Xtext+%Ddata %Mmax)k'\
+        --output=warmup$i.exectime $PGBINDIR/psql -h /tmp\
+        -p $PORT -d $DB_NAME -f $query_file  2> warmup$i.stderr > warmup$i.stdout &
+
+    let "i=$i+1"
+done
+
+# Wait for all pending streams to finish.
+echo "Wait for all pending streams to finish"
+for p in $(jobs -p); do
+  if [ $p != $PGPID ]; then
+      wait $p
+  fi
+done
+echo "Warmup done."
+
+sleep 10
+
+# Run the streams for each counter
+source "$BASEDIR/common/perf-counters-axle.sh"
+for counter in "${array[@]}"; do
+
+    i=1
+    while [ $i -le $NUMSTREAMS ]; do
+        # run the queries
+        echo "`date`: start throughput queries for stream $i for counter $counter"
+        t=$(timer)
+
+        query_file="$RUNDIR/throughput_query$i"
+        # You can't use -a and have the query redirected to a file with -o, so use -a and redirect.
+        if [ $i -eq $NUMSTREAMS ]; then
+            # Last stream, attach perf to postgres pid
+            LC_NUMERIC=C perf stat --append -o perf-stats.csv -e $counter -p $PGPID -x "," --\
+                $PGBINDIR/psql -h /tmp -p ${PORT} -d ${DB_NAME} -f ${query_file}\
+                2> stderr_stream${i}_$counter.txt > stdout_stream${i}_$counter.txt &
+        else
+            $PGBINDIR/psql -h /tmp -p ${PORT} -d ${DB_NAME} -f ${query_file}\
+                2> stderr_stream${i}_$counter.txt > stdout_stream${i}_$counter.txt &
+        fi
+
+        let "i=$i+1"
+    done
+
+    # Wait for all streams to finish.
+    for p in $(jobs -p); do
+      if [ $p != $PGPID ]; then
+          wait $p
+      fi
+    done
+
+    e_time=`$GTIME`
+    echo "`date`: end queries for counter $counter"
+    printf 'Elapsed time: %s\n' $(timer $t)
+
+    sleep 2
+
+done
+
+if [ "$SIMULATOR" = false ]; then
+    $PGBINDIR/pg_ctl stop -D $DATADIR
+    #if [ "$STAT" = false ]; then
+        #source "$BASEDIR/common/callgraph.sh"
+    #fi
+fi
