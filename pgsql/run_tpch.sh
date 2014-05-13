490db005085e8976a3575dda1901aa4e48ffadf1
Adria Armejach <adria.armejach@bsc.es>
2014-05-13 17:49:24 +0200

    Warmup only for those queries that will run. Added sample collection for ipc through time

--- pgsql/run_tpch.sh
+++ pgsql/run_tpch.sh
@@ -11,8 +11,8 @@ mkdir -p $RESULTS
 cd $RESULTS
 
 # Start and Warmup
-echo "Start and Warmup: run all queries in succession"
 if [ "$SIMULATOR" = false ]; then
+    echo "Start and Warmup: run all queries in succession"
     $PGBINDIR/postgres -D "$DATADIR" -p $PORT &
     PGPID=$!
     while ! $PGBINDIR/pg_ctl status -D $DATADIR | grep "server is running" -q; do
@@ -25,9 +25,15 @@ if [ "$SIMULATOR" = false ]; then
 
     # Warmup: run all queries in succession
     echo "Warmup: run all queries in succession"
-    /usr/bin/time -f '%e\n%Uuser %Ssystem %Eelapsed %PCPU (%Xtext+%Ddata %Mmax)k'\
-        --output=warmup.exectime $PGBINDIR/psql -h /tmp\
-        -p $PORT -d $DB_NAME < $QUERIESDIR/qall.analyze.sql  2> warmup.stderr > warmup.stdout
+    for i in $QUERIES;
+    do
+        ii=$(printf "%02d" $i)
+        /usr/bin/time -f '%e\n%Uuser %Ssystem %Eelapsed %PCPU (%Xtext+%Ddata %Mmax)k'\
+            --output=warmup_q$ii.exectime $PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME\
+            < $QUERIESDIR/q$ii.analyze.sql 2> warmup_q$ii.stderr > warmup_q$ii.stdout
+    done
+else
+    echo "Execute in Zsim:"
 fi
 
 #bq='\\"'
@@ -67,13 +73,19 @@ do
             /usr/bin/time -f '%e\n%Uuser %Ssystem %Eelapsed %PCPU (%Xtext+%Ddata %Mmax)k'\
                 --output=exectime.txt perf record -p $PGPID -s -g -m 512 --\
                 $PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -f $QUERIESDIR/q$ii.sql\
-                2> stderr.txt > stdout.txt
+                2> stderr_callgraph.txt > stdout_callgraph.txt
+
+            # Collect samples
+            perf record -p $PGPID -m 512 -e "r003C, r00C0" -F 1000\
+                -o ipc-samples.data --\
+                $PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -f $QUERIESDIR/q$ii.sql\
+                2> stderr_samples.txt > stdout_samples.txt
         else
             source "$BASEDIR/common/perf-counters-axle.sh"
             for counter in "${array[@]}"; do
                 echo "Running query $i for counters $counter."
                 # Execute queries using perf stat, repeat 5
-                LC_NUMERIC=C perf stat -r 5 -e $counter -p $PGPID -x "," --\
+                LC_NUMERIC=C perf stat -r 3 -e $counter -p $PGPID -x "," --\
                     $PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -f $QUERIESDIR/q$ii.sql\
                     2>> perf-stats.csv > stdout_$counter.txt
             done
