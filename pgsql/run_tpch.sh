b944d951548ac773f42171618b4be3b9e30b3d69
Adria Armejach <adria.armejach@bsc.es>
2014-05-13 18:25:50 +0200

    Always warmup, even with on sim

--- pgsql/run_tpch.sh
+++ pgsql/run_tpch.sh
@@ -11,40 +11,34 @@ mkdir -p $RESULTS
 cd $RESULTS
 
 # Start and Warmup
-if [ "$SIMULATOR" = false ]; then
-    echo "Start and Warmup: run all queries in succession"
-    $PGBINDIR/postgres -D "$DATADIR" -p $PORT &
-    PGPID=$!
-    while ! $PGBINDIR/pg_ctl status -D $DATADIR | grep "server is running" -q; do
-        echo "Waiting for the Postgres server to start"
-        sleep 2
-    done
+echo "Start and Warmup: run all queries in succession"
+$PGBINDIR/postgres -D "$DATADIR" -p $PORT &
+PGPID=$!
+while ! $PGBINDIR/pg_ctl status -D $DATADIR | grep "server is running" -q; do
+    echo "Waiting for the Postgres server to start"
+    sleep 2
+done
 
-    # Additional sleep time seems necessary
-    sleep 3
+# Additional sleep time seems necessary
+sleep 3
 
-    # Warmup: run all queries in succession
-    echo "Warmup: run all queries in succession"
-    for i in $QUERIES;
-    do
-        ii=$(printf "%02d" $i)
-        /usr/bin/time -f '%e\n%Uuser %Ssystem %Eelapsed %PCPU (%Xtext+%Ddata %Mmax)k'\
-            --output=warmup_q$ii.exectime $PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME\
-            < $QUERIESDIR/q$ii.analyze.sql 2> warmup_q$ii.stderr > warmup_q$ii.stdout
-    done
-else
-    echo "Execute in Zsim:"
+# Warmup: run all queries in succession, always do this, even with zsim
+echo "Warmup: run all queries in succession"
+for i in $QUERIES;
+do
+    ii=$(printf "%02d" $i)
+    /usr/bin/time -f '%e\n%Uuser %Ssystem %Eelapsed %PCPU (%Xtext+%Ddata %Mmax)k'\
+        --output=warmup_q$ii.exectime $PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME\
+        < $QUERIESDIR/q$ii.analyze.sql 2> warmup_q$ii.stderr > warmup_q$ii.stdout
+done
+
+if [ "$SIMULATOR" = true ]; then
+    echo "Execute in Zsim: stop postgres server"
+    $PGBINDIR/pg_ctl stop -D $DATADIR
+    sleep 15
 fi
 
-#bq='\\"'
-#damp='\&\&'
-#sq="'"
-#simpgstart="$PGBINDIR/pg_ctl -o ${sq}-p ${PORT}${sq} -w start -D $DATADIR"
-#simpgquery="$PGBINDIR/psql -h /tmp -p $PORT -d $DB_NAME -f"
-#simpgstop="$PGBINDIR/pg_ctl -o ${sq}-p ${PORT}${sq} -w stop -D $DATADIR"
-#echo $simpgstart
-#echo $simpgquery
-#echo $simpgstop
+# For each query, run...
 for i in $QUERIES;
 do
     echo "Running query: $i"
@@ -61,12 +55,6 @@ do
         cp $PGSIMSCRIPT run-$ii.sh
         sed -i "s/%QNUM%/$ii/g" run-$ii.sh
         source ./run-$ii.sh
-        #pattern='command = "";'
-        #cmd="command = \"bash -c ${bq}$PGBINDIR/postgres --single -j -D $DATADIR $DB_NAME < $QUERIESDIR/q$ii.sql${bq}\";"
-        #cmd="command = \"bash -c ${bq}$simpgstart $damp $simpgquery $QUERIESDIR/q$ii.sql $damp $simpgstop${bq}\";"
-        #echo $cmd
-        #sed -i "s#${pattern}#${cmd}#g" in.cfg
-        #$ZSIMPATH/build/opt/zsim in.cfg 2> term.stderr > term.stdout
     else
         if [ "$STAT" = false ]; then
             # Execute each query once
