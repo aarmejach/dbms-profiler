ea4be51de274dded08b12712ab1fc5bf46f8afc8
Adria Armejach <adria.armejach@bsc.es>
2014-07-18 15:40:35 +0200

    Creat DB script for TPC-C

--- pgsql/create_database_dbt2.sh
+++ pgsql/create_database_dbt2.sh
@@ -3,45 +3,93 @@
 # Install teardown() function to kill any lingering jobs
 # Check if postgres is running
 source "$BASEDIR/$DATABASE/common.sh"
-
-# Compile the benchmark
-cd $BENCHDIR
-cmake -G "Unix Makefiles" -DDBMS=pgsql -DCMAKE_BUILD_TYPE=Debug
-make
-
-cd storedproc/pgsql/c
-make
-make install #to shared folder, check
-cd $BASEDIR
+test -z "${DEBUG-}" && trap "teardown" EXIT
 
 # Current time
 t=$(timer)
 
-TIME=`date`
-echo $PATH
+# Prepare benchmark
+cd $BENCHDIR
+cmake -G "Unix Makefiles" -DDBMS=pgsql -DCMAKE_BUILD_TYPE=Debug
+make
 
-# Create db and populate
-sudo -u $PGUSER env PATH=$PATH dbt2-pgsql-build-db -w $SCALE -l $PORT
+cd ${BENCHDIR}/storedproc/pgsql/c
+make
+make install #to postgres shared folder
 
-# Configuration for query execution
-query_configuration="
-checkpoint_timeout = 600s
-work_mem = 12GB                      #2 to 16GB
-effective_cache_size = 192GB        #3/4 of total RAM (192GB)
-default_statistics_target = 5000    #requires analyze to take effect
-maintenance_work_mem = 32MB
+cd $BASEDIR
+
+# Check if pgsql port is free
+do_check_port
+
+# Check if pgsql already running in datadir
+do_check_datadir
+
+#initdb
+${PGBINDIR}/initdb -D "${DATADIR}" --locale=C
+
+# Start Postgres server
+do_start_postgres
+
+#createdb
+${PGBINDIR}/createdb -h /tmp -p ${PORT} $DB_NAME --locale=C
+
+# Change postgres configuration for data loading
+loading_configuration="
+checkpoint_segments = 300
+checkpoint_timeout = 3600s
+checkpoint_completion_target = 0.9
+checkpoint_timeout = $CHECKPOINT_TIMEOUT
+work_mem = $WORK_MEM
+effective_cache_size = $EFFECTIVE_CACHE
+shared_buffers = $SHARED_BUFFERS
+default_statistics_target = $STATISTICS_TARGET
+maintenance_work_mem = $MAINTAINANCE_MEM
 "
+echo "$loading_configuration" | tee -a $DATADIR/postgresql.conf
+$PGBINDIR/pg_ctl reload -D $DATADIR
 
-echo "$query_configuration" | sudo -u $PGUSER tee -a $DATADIR/postgresql.conf
-sudo -u $PGUSER env PATH=$PATH pg_ctl reload -D $DATADIR
+# Create tables
+env PGBINDIR=$PGBINDIR DBT2DBNAME=$DB_NAME $BENCHDIR/bin/pgsql/dbt2-pgsql-create-tables -l ${PORT}
 
-# Disable transparent huge pages
-echo never | sudo tee /sys/kernel/mm/transparent_hugepage/defrag
+# Data generation
+env PGPORT=${PORT} PGDATABASE=$DB_NAME $BENCHDIR/bin/dbt2-datagen --direct -w ${SCALE} --pgsql
 
-echo "Stop the postgres server"
-sudo -u $PGUSER env PATH=$PATH pg_ctl stop -D $DATADIR
+# Create Indexes
+env PGBINDIR=$PGBINDIR DBT2DBNAME=$DB_NAME $BENCHDIR/bin/pgsql/dbt2-pgsql-create-indexes -l ${PORT}
 
-sudo -u $PGUSER cp -a $DATADIR $DATADIR-template
+# Load stored functions
+PSQL="${PGBINDIR}/psql -p ${PORT} -e -d ${DB_NAME}"
+SHAREDIR=`${PGBINDIR}/pg_config --sharedir`
+echo "loading C stored functions..."
+${PSQL} -f ${SHAREDIR}/contrib/delivery.sql
+${PSQL} -f ${SHAREDIR}/contrib/new_order.sql
+${PSQL} -f ${SHAREDIR}/contrib/order_status.sql
+${PSQL} -f ${SHAREDIR}/contrib/payment.sql
+${PSQL} -f ${SHAREDIR}/contrib/stock_level.sql
 
+# Set seed
+${PGBINDIR}/psql -p ${PORT} -e -d ${DB_NAME} -c "SELECT setseed(0);"
+
+# VACUUM FULL ANALYZE: Build optimizer statistics for newly-created
+# tables. The VACUUM FULL is probably unnecessary; we want to scan the
+# heap and update the commit-hint bits on each new tuple, but a regular
+# VACUUM ought to suffice for that.
+${PGBINDIR}/vacuumdb -p ${PORT} -z -f -d ${DB_NAME}
+
+# Stop pg server
+do_stop_postgres
+
+# Load configuration
+benchmark_configuration="
+track_activities = off
+track_counts = off
+track_io_timing = off
+update_process_title = off
+autovacuum = off
+"
+echo "$benchmark_configuration" | tee -a $DATADIR/postgresql.conf
+
+# Go to basedir and print elapsed time
 cd $BASEDIR
 printf 'Elapsed time: %s\n' $(timer $t)
