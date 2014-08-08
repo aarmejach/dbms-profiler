d8fff7025f6273b2a624eca4a6fd0c08c6633ad4
Adria Armejach <adria.armejach@bsc.es>
2014-08-08 17:53:44 +0200

    Added scripts to push and build in the cluster

---
+++ scripts/cluster/cluster_build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+NAS_HOME=/scratch/nas/1/adria
+cd $NAS_HOME
+
+# Zsim paths
+export PINPATH=$NAS_HOME/zsim/pin_kit
+export NVMAINPATH=$NAS_HOME/zsim/nvmain
+export ZSIMPATH=$NAS_HOME/zsim
+export PGPATH=$NAS_HOME/postgres
+export BOOST=$NAS_HOME/boost
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$BOOST/stage/lib
+
+# Set PATH for postgresql
+export PATH=$PGPATH/build/bin:$PATH
+
+# gcc
+export CC=gcc
+export CXX=g++
+
+CORES=$(cat /proc/cpuinfo | grep processor -c)
+if [ $CORES -eq 1 ]; then
+    # single-core node, compile on some other node!
+    echo "On single-core node $(hostname). Jumping to another node."
+    qrsh $NAS_HOME/cluster_build.sh
+    exit $?
+else
+    echo "Building on: $(hostname), with $CORES cores"
+fi
+
+# Compile zsim
+cd zsim
+scons --r -j$CORES
+echo "ZSim compilation done."
+echo "---------------------------------"
+
+# Compile postgres
+cd ../postgres
+./install.sh
+echo "Postgres compilation done."
+echo "---------------------------------"
---
+++ scripts/cluster/cluster_push.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+BASEDIR=$(dirname "$0")
+BASEDIR=$(cd "$BASEDIR"; pwd)
+
+USER=adria
+SERVER=gaudi
+SERVER_HOME=/scratch/nas/1/adria
+SERVER_DIR=$USER@$SERVER:$SERVER_HOME
+
+#boost
+rsync -aP --delete --exclude 'libs' --exclude 'tools' --exclude 'doc' $BOOST $SERVER_DIR/
+
+#postgres
+rsync -aP --delete --exclude 'build' --exclude '.git' $PGPATH $SERVER_DIR/
+
+#zsim
+rsync -aP --delete --exclude 'build' --exclude '.git' --exclude 'DRAMSim2/' --exclude 'mcpat*' --exclude 'nvmain/.hg' --exclude 'nvmain/build' --exclude 'results' $ZSIMPATH $SERVER_DIR/
+
+#install
+scp $BASEDIR/cluster_build.sh $SERVER_DIR
+
+ssh $USER@$SERVER $SERVER_HOME/cluster_build.sh
