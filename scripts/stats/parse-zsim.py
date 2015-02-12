dffdf79bde195b3ec3816fc12d81baf6c3810d78
Adria Armejach <adria.armejach@bsc.es>
2015-02-12 17:50:36 +0100

    Fixes and better error handling for missing result files

--- scripts/stats/parse-zsim.py
+++ scripts/stats/parse-zsim.py
@@ -12,10 +12,13 @@ def parse_h5file(file):
     res = {}
 
     f = h5py.File(file, 'r')
+    #print "parsing ", file
 
     # Get the single dataset in the file
     try:
         dset = f["stats"]["root"]
+        if dset.size == 0:
+            raise ValueError
     except ValueError:
         print "ERROR reading file: " + file
         for key in zsimstatsdict:
@@ -41,6 +44,7 @@ def toFloat(a):
 
 def parse_nvmainFile(file):
     res = {}
+    #print "parsing ", file
 
     array = open(file).read().strip().split('\n')
 
@@ -56,9 +60,11 @@ def get_results(d):
     results = {}
     for root, sfs, fs in os.walk(folder_results + d):
         for f in fs:
+            #print f
             tmp = f.split('_')[0:3]
             # change query format
-            tmp[1] = "%02d" % int(tmp[1])
+            if 'tpch' in tmp[0]:
+                tmp[1] = "%02d" % int(tmp[1])
 
             if 'zsim-ev.h5' in f:
                 dict = parse_h5file(os.path.join(root, f))
@@ -111,26 +117,59 @@ def data_to_csv(results):
         column_names.append(elem)
     data.append(column_names)
 
-    # Add rows one by one
-    for key in sorted(results):
-        row = []
-        row.append(key[0]) # bench
-        row.append(key[1])# query
-        row.append(key[2]) # scale
+    # Add rows one by one, check all possible workloads and add empty lines for the
+    # ones that do not have results. Issue a warning if that is the case.
+    for benchtype in benchmarks:
+        benchlist = benchmarks[benchtype]
+        for bench in benchlist:
+            # change query format
+            if 'tpch' in benchtype:
+                bench = "%02d" % int(bench)
+            key = (benchtype, bench, '100')
+            row = []
+            row.append(key[0]) # bench
+            row.append(key[1])# query
+            row.append(key[2]) # scale
+            if key in results:
+                # Get event dict and iterate over sorted keys
+                dict = results.get(key)
+                for k in sorted(dict):
+                    row.append(dict[k])
+                data.append(row)
+            else:
+                row.append([0] * len(results[results.keys()[0]]))
+                data.append(row)
+                print "WARN: Missing benchmark " + benchtype + " " + bench + " in file " + csv_file.split("/")[-1]
+
+
+    #for key in sorted(results):
+        #row = []
+        #row.append(key[0]) # bench
+        #row.append(key[1])# query
+        #row.append(key[2]) # scale
 
         # Get event dict and iterate over sorted keys
-        dict = results.get(key)
-        for k in sorted(dict):
-            row.append(dict[k])
-        data.append(row)
+        #dict = results.get(key)
+        #for k in sorted(dict):
+            #row.append(dict[k])
+        #data.append(row)
 
     with open(csv_file, 'wb') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         spamwriter.writerows(data)
 
 
+def parse_periodic_stats(d):
+    for root, sfs, fs in os.walk(folder_results + d):
+        print root
+        for f in fs:
+            if 'zsim.h5' in f:
+                print f
+                os.system("python common/parse-ipc-samples-zsim.py %(IN)s > %(OUT)s"%{'IN':os.path.join(root, f), 'OUT':os.path.join(root,f)+'.csv'})
+
+
 def _main():
-    global zsimstatsdict, nvmainstatsdict, csv_file, folder_results
+    global zsimstatsdict, nvmainstatsdict, csv_file, folder_results, benchmarks
 
     # Go to profiler's base dir
     os.chdir(os.path.dirname(os.path.realpath(__file__))+ '/../..')
@@ -140,7 +179,19 @@ def _main():
     folder_results = os.getcwd() + '/' + "results-zsim/"
     directories=[d for d in os.listdir(folder_results) if os.path.isdir(folder_results + d)]
 
+    benchmarks = { # 447.dealII and 481.wrf not working
+    'spec' : "400.perlbench 403.gcc 416.gamess 433.milc 435.gromacs 437.leslie3d 445.gobmk 450.soplex 454.calculix 458.sjeng 462.libquantum 465.tonto 471.omnetpp 483.xalancbmk 401.bzip2 410.bwaves 429.mcf 434.zeusmp 436.cactusADM 444.namd 453.povray 456.hmmer 459.GemsFDTD 464.h264ref 470.lbm 473.astar 482.sphinx3".split(),
+    'spec-r' : "400.perlbench 403.gcc 416.gamess 433.milc 435.gromacs 437.leslie3d 445.gobmk 450.soplex 454.calculix 458.sjeng 462.libquantum 465.tonto 471.omnetpp 483.xalancbmk 401.bzip2 410.bwaves 429.mcf 434.zeusmp 436.cactusADM 444.namd 453.povray 456.hmmer 459.GemsFDTD 464.h264ref 470.lbm 473.astar 482.sphinx3".split(),
+    'tpch' : "2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22".split(),
+    'dbt3' : "1 2 4 8".split(),
+    #'dbt2' : "16 32 64".split()
+    }
+
     for d in directories:
+
+        # Parse periodic stats for ipc sampling
+        #parse_periodic_stats(d)
+
         csv_file = os.getcwd() + '/' + "results/zsim-data-" + d + ".csv"
 
         # Define stats of interest and gather results
@@ -154,7 +205,8 @@ def _main():
               }
         nvmainstatsdict = {
               'DRC'    : ['totalPower', 'rb_hits', 'rb_miss', 'drc_hits', 'drc_miss'],
-              'FRFCFS' : ['totalPower', 'rb_hits', 'rb_miss'],
+              'PredictorDRC' : ['truePredictions', 'falsePredictions'],
+              'FRFCFS' : ['totalPower', 'rb_hits', 'rb_miss']
               }
         results = get_results(d)
         #pprint(results)
@@ -183,6 +235,9 @@ def _main():
         # DRC
         add_counter(results, 'drc_hitrate', '$DRC_drc_hits / ( $DRC_drc_hits + $DRC_drc_miss )')
         add_counter(results, 'drc_rbhitrate', '$DRC_rb_hits / ( $DRC_rb_hits + $DRC_rb_miss )')
+
+        #PredictorDRC
+        add_counter(results, 'predictor_rate', '$PredictorDRC_truePredictions / ( $PredictorDRC_truePredictions + $PredictorDRC_falsePredictions )')
 
         #FRFCFS
         add_counter(results, 'frfcfs_rbhitrate', '$FRFCFS_rb_hits / ( $FRFCFS_rb_hits + $FRFCFS_rb_miss )')
