91af0ca033ceeada9eaa78f01bbb6a073fa2b17e
Adria Armejach <adria.armejach@bsc.es>
2015-02-26 19:22:21 +0100

    Update results checker script

--- scripts/stats/results-check.sh
+++ scripts/stats/results-check.sh
@@ -2,8 +2,70 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-CONFIGS="baseline base3dcachepred base3dcacheunison"
-APPS="spec_400.perlbench spec_403.gcc spec_416.gamess spec_433.milc spec_435.gromacs spec_437.leslie3d spec_445.gobmk spec_450.soplex spec_454.calculix spec_458.sjeng spec_462.libquantum spec_465.tonto spec_471.omnetpp spec_483.xalancbmk spec_401.bzip2 spec_410.bwaves spec_429.mcf spec_434.zeusmp spec_436.cactusADM spec_444.namd spec_453.povray spec_456.hmmer spec_459.GemsFDTD spec_464.h264ref spec_470.lbm spec_473.astar spec_482.sphinx3 spec-r_400.perlbench spec-r_403.gcc spec-r_416.gamess spec-r_433.milc spec-r_435.gromacs spec-r_437.leslie3d spec-r_445.gobmk spec-r_450.soplex spec-r_454.calculix spec-r_458.sjeng spec-r_462.libquantum spec-r_465.tonto spec-r_471.omnetpp spec-r_483.xalancbmk spec-r_401.bzip2 spec-r_410.bwaves spec-r_429.mcf spec-r_434.zeusmp spec-r_436.cactusADM spec-r_444.namd spec-r_453.povray spec-r_456.hmmer spec-r_459.GemsFDTD spec-r_464.h264ref spec-r_470.lbm spec-r_473.astar spec-r_482.sphinx3 tpch_2 tpch_3 tpch_4 tpch_5 tpch_6 tpch_7 tpch_8 tpch_9 tpch_10 tpch_11 tpch_12 tpch_13 tpch_14 tpch_15 tpch_16 tpch_17 tpch_18 tpch_19 tpch_20 tpch_21 tpch_22 dbt3_1 dbt3_2 dbt3_4 dbt3_8"
+CONFIGS="base alloy unison"
+APPS="
+spec-mix_403.gcc-410.bwaves-429.mcf-482.sphinx3-473.astar-403.gcc-433.milc-410.bwaves_100
+spec-mix_403.gcc-429.mcf-459.GemsFDTD-471.omnetpp-470.lbm-450.soplex-459.GemsFDTD-470.lbm_100
+spec-mix_403.gcc-433.milc-437.leslie3d-429.mcf-403.gcc-462.libquantum-459.GemsFDTD-473.astar_100
+spec-mix_403.gcc-433.milc-450.soplex-410.bwaves-470.lbm-437.leslie3d-403.gcc-459.GemsFDTD_100
+spec-mix_403.gcc-433.milc-450.soplex-410.bwaves-473.astar-433.milc-450.soplex-470.lbm_100
+spec-mix_403.gcc-437.leslie3d-403.gcc-450.soplex-473.astar-433.milc-410.bwaves-470.lbm_100
+spec-mix_403.gcc-437.leslie3d-462.libquantum-459.GemsFDTD-403.gcc-410.bwaves-429.mcf-482.sphinx3_100
+spec-mix_403.gcc-459.GemsFDTD-433.milc-437.leslie3d-437.leslie3d-450.soplex-462.libquantum-470.lbm_100
+spec-mix_403.gcc-462.libquantum-403.gcc-433.milc-450.soplex-429.mcf-403.gcc-433.milc_100
+spec-mix_403.gcc-482.sphinx3-429.mcf-482.sphinx3-471.omnetpp-410.bwaves-429.mcf-482.sphinx3_100
+spec-mix_410.bwaves-459.GemsFDTD-470.lbm-462.libquantum-473.astar-450.soplex-470.lbm-482.sphinx3_100
+spec-mix_429.mcf-471.omnetpp-459.GemsFDTD-470.lbm-482.sphinx3-450.soplex-459.GemsFDTD-470.lbm_100
+spec-mix_433.milc-429.mcf-403.gcc-433.milc-462.libquantum-482.sphinx3-433.milc-470.lbm_100
+spec-mix_433.milc-437.leslie3d-433.milc-403.gcc-462.libquantum-429.mcf-470.lbm-482.sphinx3_100
+spec-mix_433.milc-437.leslie3d-462.libquantum-471.omnetpp-410.bwaves-473.astar-403.gcc-462.libquantum_100
+spec-mix_433.milc-450.soplex-429.mcf-473.astar-482.sphinx3-403.gcc-429.mcf-482.sphinx3_100
+spec-mix_433.milc-450.soplex-473.astar-482.sphinx3-403.gcc-410.bwaves-450.soplex-410.bwaves_100
+spec-mix_433.milc-462.libquantum-471.omnetpp-462.libquantum-471.omnetpp-410.bwaves-450.soplex-429.mcf_100
+spec-mix_437.leslie3d-433.milc-470.lbm-437.leslie3d-462.libquantum-471.omnetpp-429.mcf-473.astar_100
+spec-mix_437.leslie3d-471.omnetpp-429.mcf-470.lbm-433.milc-471.omnetpp-433.milc-437.leslie3d_100
+spec-mix_437.leslie3d-471.omnetpp-429.mcf-473.astar-482.sphinx3-471.omnetpp-450.soplex-459.GemsFDTD_100
+spec-mix_450.soplex-429.mcf-470.lbm-403.gcc-433.milc-471.omnetpp-459.GemsFDTD-450.soplex_100
+spec-mix_450.soplex-462.libquantum-471.omnetpp-429.mcf-470.lbm-473.astar-482.sphinx3-470.lbm_100
+spec-mix_450.soplex-462.libquantum-473.astar-462.libquantum-459.GemsFDTD-462.libquantum-473.astar-482.sphinx3_100
+spec-mix_459.GemsFDTD-433.milc-471.omnetpp-410.bwaves-482.sphinx3-410.bwaves-429.mcf-482.sphinx3_100
+spec-mix_462.libquantum-410.bwaves-459.GemsFDTD-470.lbm-437.leslie3d-471.omnetpp-433.milc-450.soplex_100
+spec-mix_462.libquantum-410.bwaves-470.lbm-473.astar-433.milc-459.GemsFDTD-473.astar-482.sphinx3_100
+spec-mix_470.lbm-433.milc-437.leslie3d-470.lbm-482.sphinx3-462.libquantum-471.omnetpp-470.lbm_100
+spec-mix_471.omnetpp-410.bwaves-459.GemsFDTD-437.leslie3d-470.lbm-433.milc-471.omnetpp-410.bwaves_100
+spec-mix_471.omnetpp-459.GemsFDTD-403.gcc-450.soplex-410.bwaves-429.mcf-429.mcf-470.lbm_100
+spec-mix_471.omnetpp-470.lbm-433.milc-437.leslie3d-459.GemsFDTD-450.soplex-429.mcf-473.astar_100
+spec-mix_471.omnetpp-482.sphinx3-410.bwaves-473.astar-437.leslie3d-429.mcf-470.lbm-473.astar_100
+spec-r_403.gcc_100
+spec-r_410.bwaves_100
+spec-r_429.mcf_100
+spec-r_433.milc_100
+spec-r_437.leslie3d_100
+spec-r_450.soplex_100
+spec-r_459.GemsFDTD_100
+spec-r_462.libquantum_100
+spec-r_470.lbm_100
+spec-r_471.omnetpp_100
+spec-r_473.astar_100
+spec-r_482.sphinx3_100
+spec_403.gcc_100
+spec_410.bwaves_100
+spec_429.mcf_100
+spec_433.milc_100
+spec_437.leslie3d_100
+spec_450.soplex_100
+spec_459.GemsFDTD_100
+spec_462.libquantum_100
+spec_470.lbm_100
+spec_471.omnetpp_100
+spec_473.astar_100
+spec_482.sphinx3_100
+"
+#spec_400.perlbench spec_403.gcc spec_416.gamess spec_433.milc spec_435.gromacs spec_437.leslie3d spec_445.gobmk spec_450.soplex spec_454.calculix spec_458.sjeng spec_462.libquantum spec_465.tonto spec_471.omnetpp spec_483.xalancbmk spec_401.bzip2 spec_410.bwaves spec_429.mcf spec_434.zeusmp spec_436.cactusADM spec_444.namd spec_453.povray spec_456.hmmer spec_459.GemsFDTD spec_464.h264ref spec_470.lbm spec_473.astar spec_482.sphinx3
+#spec-r_400.perlbench spec-r_403.gcc spec-r_416.gamess spec-r_433.milc spec-r_435.gromacs spec-r_437.leslie3d spec-r_445.gobmk spec-r_450.soplex spec-r_454.calculix spec-r_458.sjeng spec-r_462.libquantum spec-r_465.tonto spec-r_471.omnetpp spec-r_483.xalancbmk spec-r_401.bzip2 spec-r_410.bwaves spec-r_429.mcf spec-r_434.zeusmp spec-r_436.cactusADM spec-r_444.namd spec-r_453.povray spec-r_456.hmmer spec-r_459.GemsFDTD spec-r_464.h264ref spec-r_470.lbm spec-r_473.astar spec-r_482.sphinx3
+#tpch_2 tpch_3 tpch_4 tpch_5 tpch_6 tpch_7 tpch_8 tpch_9 tpch_10 tpch_11 tpch_12 tpch_13 tpch_14 tpch_15 tpch_16 tpch_17 tpch_18 tpch_19 tpch_20 tpch_21 tpch_22
+#dbt3_1 dbt3_2 dbt3_4 dbt3_8"
+
 BASE=tests_zsim_
 OUTFILE=/dev/stdout #$DIR/results_check.txt
 RESDIR="/home/aarmejach/projects/AXLE/dbms-profiler/results-zsim"
@@ -18,7 +80,8 @@ for CONFIG in $CONFIGS; do
     cd ${RESDIR}/${BASE}${CONFIG}
 
     for APP in $APPS; do
-        file=${APP}_100_
+        #file=${APP}_100_
+        file=${APP}_
 
         #Check if simterm file exists
         if [ ! -e ${file}simterm.txt ]; then
