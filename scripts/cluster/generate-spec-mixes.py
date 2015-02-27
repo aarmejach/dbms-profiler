6fd84d6e5f646a3f7bf80f7c0c34e493a5c11c18
Adria Armejach <adria.armejach@bsc.es>
2015-02-27 17:54:43 +0100

    Remove bwave from spec mixes and recreating 64 mixes

--- scripts/cluster/generate-spec-mixes.py
+++ scripts/cluster/generate-spec-mixes.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
 
-from itertools import combinations
+from itertools import combinations, combinations_with_replacement
 import random
 
-APPSa = "403.gcc 433.milc 437.leslie3d 450.soplex 462.libquantum 471.omnetpp 410.bwaves 429.mcf 459.GemsFDTD 470.lbm 473.astar 482.sphinx3".split()
-APPSb = "403.gcc 433.milc 437.leslie3d 450.soplex 462.libquantum 471.omnetpp 410.bwaves 429.mcf 459.GemsFDTD 470.lbm 473.astar 482.sphinx3".split()
-APPSc = "403.gcc 433.milc 437.leslie3d 450.soplex 462.libquantum 471.omnetpp 410.bwaves 429.mcf 459.GemsFDTD 470.lbm 473.astar 482.sphinx3".split()
+APPSa = "403.gcc 433.milc 437.leslie3d 450.soplex 462.libquantum 471.omnetpp 429.mcf 459.GemsFDTD 470.lbm 473.astar 482.sphinx3".split()
+#APPSb = "403.gcc 433.milc 437.leslie3d 450.soplex 462.libquantum 471.omnetpp 429.mcf 459.GemsFDTD 470.lbm 473.astar 482.sphinx3".split()
+#APPSc = "403.gcc 433.milc 437.leslie3d 450.soplex 462.libquantum 471.omnetpp 429.mcf 459.GemsFDTD 470.lbm 473.astar 482.sphinx3".split()
 
-APPS = APPSa + APPSb + APPSc
+#APPS = APPSa + APPSb + APPSc
+APPS = APPSa
 
-MIXES = list(combinations(APPS, 8))
+#MIXES = list(combinations(APPS, 8))
+MIXES = list(combinations_with_replacement(APPS, 8))
 print len(MIXES)
 
-NUMMIXES = 32
+NUMMIXES = 64
 
 OUTPUT = []
 
