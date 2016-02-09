a91a2a53e0db2a429b96b01f5397020f4319fbf5
Adria Armejach <adria.armejach@bsc.es>
2016-02-09 14:59:24 +0100

    Back to velox

--- pmfs-pull.sh
+++ pmfs-pull.sh
@@ -3,12 +3,12 @@
 BASEDIR=$(dirname "$0")
 BASEDIR=$(cd "$BASEDIR"; pwd)
 
-USER=$USER
+USER=aarmejach
 SERVER=84.88.52.252
 SERVER_HOME=/home/$USER
 SERVER_DIR=$USER@$SERVER:$SERVER_HOME
 RESULTS_DIR=dbms-profiler/results/
 
-rsync -aP $SERVER_DIR/$RESULTS_DIR results-pmfs
+rsync -aP --exclude="perf.data" $SERVER_DIR/$RESULTS_DIR results-pmfs
 
 #ssh $SERVER $SERVER_HOME/script.sh
